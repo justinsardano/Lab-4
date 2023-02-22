3debb56a82ae627cbeec1fa2113bc6d2ac6bbc0a
justinsardano <124173790+justinsardano@users.noreply.github.com>
2023-02-22 10:04:52 -0500

    Update functionshapes.js
    https://editor.p5js.org/justin.sardano/sketches/n-J8msgTW

--- functionshapes.js
+++ functionshapes.js
@@ -5,19 +5,21 @@ function setup() {
 function draw() {
   background(220);
   
-  myTriangle(50,50,50,50,50,50)
-  myCircle()
-  myRect()
+  myTriangle(50,50,50,50,50,50,"red")
+  myCircle(0,0,0,"blue")
+ myArc(0,0,0,0,0,0,"purple")
 }
-function myTriangle(x1,y1,x2,y2,x3,y3){
-triangle (20,40,60,4,50,100)
+function myTriangle(x1,y1,x2,y2,x3,y3,myColor){
+fill(myColor)
+  triangle (20,40,60,4,50,100)
 
 }
-function myCircle(x,y,diameter){
-ellipse(100,100,100)
+function myCircle(x,y,diameter,myColor){
+  fill(myColor)
+  ellipse(100,100,100)
 
 }
-function myRect(x,y,rectWidth,rectHeight){
-  rect(200,200,50,300)
-  
+function myArc(x,y,x2,y2,x3,y3,myColor){
+ fill(myColor)
+  arc(300,300,100,100,100,10)
 }
